610203a324b217c1feaa318fa47a4572d04d137d
Thomas Schmid <tom@binary-kitchen.de>
2019-10-09 21:39:50 +0200

    bmi088_accel: reorder code and make it compile again
    Signed-off-by: Thomas Schmid <tom@binary-kitchen.de>

--- drivers/sensor/bmi088_accel/bmi088_acc.h
+++ drivers/sensor/bmi088_accel/bmi088_acc.h
@@ -4,6 +4,7 @@
 #include <zephyr/types.h>
 #include <zephyr.h>
 #include <device.h>
+#include <drivers/sensor.h>
 
 #define BMI088_REG_ACC_SOFTRESET       (u8_t) 0x7E
 #define BMI088_REG_ACC_PWR_CTRL        (u8_t) 0x7D
@@ -72,23 +73,23 @@
 #define BMI088_ACC_RANGE_24            (u8_t) 0x03
 
 #if defined(CONFIG_BMI088_ACCEL_RANGE_3G)
-#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_CONF_RANGE_3
+#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_RANGE_3
 #elif defined(CONFIG_BMI0088_ACCEL_RANGE_6G)
-#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_CONF_RANGE_6
+#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_RANGE_6
 #elif defined(CONFIG_BMI0088_ACCEL_RANGE_12G)
-#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_CONF_RANGE_12
+#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_RANGE_12
 #elif defined(CONFIG_BMI0088_ACCEL_RANGE_24G)
-#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_CONF_RANGE_24
+#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_RANGE_24
 #else
-#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_CONF_RANGE_24
+#   define BMI088_ACC_DEFAULT_RANGE           BMI088_ACC_RANGE_24
 #endif
 
 #if defined(CONFIG_BMI088_ACCEL_BANDWITH_ODR_1)
 #   define BMI088_ACC_DEFAULT_BANDWIDTH       BMI088_ACC_CONF_BANDWIDTH_OSR1
 #elif defined(CONFIG_BMI088_ACCEL_BANDWITH_ODR_2)
-#   define BMI088_ACC_DEFAULT_BANDWIDTH       BMI088_ACC_CONF_BANDWIDTH_OSR2 
+#   define BMI088_ACC_DEFAULT_BANDWIDTH       BMI088_ACC_CONF_BANDWIDTH_OSR2
 #elif defined(CONFIG_BMI088_ACCEL_BANDWITH_ODR_4)
-#   define BMI088_ACC_DEFAULT_BANDWIDTH       BMI088_ACC_CONF_BANDWIDTH_OSR4 
+#   define BMI088_ACC_DEFAULT_BANDWIDTH       BMI088_ACC_CONF_BANDWIDTH_OSR4
 #else
 #   define BMI088_ACC_DEFAULT_BANDWIDTH       BMI088_ACC_CONF_BANDWIDTH_OSR1
 #endif
@@ -110,50 +111,50 @@
 #elif defined(CONFIG_BMI088_ACCEL_ODR_1600)
 #   define BMI088_ACC_DEFAULT_ODR          BMI088_ACC_CONF_ODR_1600
 #else
-#   define BMI088_ACC_DEFAULT_ODR          BMI088_ACCEL_ODR_200
-#endif   
+#   define BMI088_ACC_DEFAULT_ODR          BMI088_ACC_CONF_ODR_200
+#endif
 
 
 struct bmi088_acc_data {
-    struct device *bmi088_com_dev;
-    const struct bmi088_transfer_function *tf;
+	struct device *bmi088_com_dev;
+	const struct bmi088_transfer_function *tf;
 
-    u8_t acc_odr;
-    u8_t acc_bandwith;
-    u8_t acc_range;
+	u8_t acc_odr;
+	u8_t acc_bandwidth;
+	u8_t acc_range;
 
-    union {
-        struct {
-            s16_t x_value;
-            s16_t y_value;
-            s16_t z_value;
-        }__attribute__((packed));
-        s16_t array[3];
-    } acc_raw_values;
+	union {
+		struct {
+			s16_t x_value;
+			s16_t y_value;
+			s16_t z_value;
+		} __packed;
+		s16_t array[3];
+	} acc_raw_values;
 
-    union {
-        struct {
-            struct sensor_value x_value;
-            struct sensor_value y_value;
-            struct sensor_value z_value;
-        }__attribute__((packed));
-        struct sensor_value array[3];
-    } acc_values;
+	union {
+		struct {
+			struct sensor_value x_value;
+			struct sensor_value y_value;
+			struct sensor_value z_value;
+		} __packed;
+		struct sensor_value array[3];
+	} acc_values;
 
-    u16_t temp_raw;
-    struct sensor_value temp;
+	u16_t temp_raw;
+	struct sensor_value temp;
 };
 
 struct bmi088_transfer_function {
-    int (*read_register)(struct bmi088_data *data, u16_t reg,
-                         int count, void *val);
-    int (*write_register)(struct bmi088_data *data, u16_t reg,
-                         int count, void *val);
+	int (*read_register)(struct bmi088_acc_data *data, u16_t reg,
+			     int count, void *val);
+	int (*write_register)(struct bmi088_acc_data *data, u16_t reg,
+			      int count, void *val);
 };
 
 struct bmi088_acc_config {
-    char *bmi088_com_dev_name;
-}
+	char *bmi088_com_dev_name;
+};
 
 int bmi088_acc_spi_init(struct bmi088_acc_data *data);
 
--- drivers/sensor/bmi088_accel/bmi088_acc_spi.c
+++ drivers/sensor/bmi088_accel/bmi088_acc_spi.c
@@ -1,6 +1,7 @@
 #include "bmi088_acc.h"
 #include <drivers/spi.h>
 #include <logging/log.h>
+#include <drivers/sensor.h>
 
 LOG_MODULE_DECLARE(bmi088_accel);
 
@@ -10,125 +11,123 @@ static struct spi_cs_control bmi088_acc_cs_ctrl;
 #endif
 
 static struct spi_config bmi088_acc_spi_conf = {
-    .frequency = DT_INST_0_BOSCH_BMI088_ACCEL_SPI_MAX_FREQUENCY,
-    .operation = (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) |
-                  SPI_MODE_CPOL | SPI_MODE_CPHA |
-                  SPI_TRANSFER_MSB |
-                  SPI_LINES_SINGLE),
-    .slave     = DT_INST_0_BOSCH_BMI088_ACCEL_BASE_ADDRESS,
-    .cs        = SPI_CS,
+	.frequency = DT_INST_0_BOSCH_BMI088_ACCEL_SPI_MAX_FREQUENCY,
+	.operation = (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) |
+		      SPI_MODE_CPOL | SPI_MODE_CPHA |
+		      SPI_TRANSFER_MSB |
+		      SPI_LINES_SINGLE),
+	.slave = DT_INST_0_BOSCH_BMI088_ACCEL_BASE_ADDRESS,
+	.cs = SPI_CS,
 };
 
 int read_register(struct bmi088_acc_data *data, u16_t reg, int count, void *val)
 {
-    int res;
-    u8_t num_pad = 1;
-    struct spi_config *spi_conf = &bmi088_gyro_spi_conf;
-    struct spi_buf_set rx_buf_set;
-    struct spi_buf_set tx_buf_set;
-    struct spi_buf tx_buf[2];
-    struct spi_buf rx_buf[2];
+	int res;
+	struct spi_buf_set rx_buf_set;
+	struct spi_buf_set tx_buf_set;
+	struct spi_buf tx_buf[2];
+	struct spi_buf rx_buf[2];
 
-    rx_buf_set.buffers = rx_buf;
-    tx_buf_set.buffers = tx_buf;
+	rx_buf_set.buffers = rx_buf;
+	tx_buf_set.buffers = tx_buf;
 
-    reg |= (1 << 7);
+	reg |= (1 << 7);
 
-    rx_buf[0].buf = NULL;
-    rx_buf[0].len = 2;
-    rx_buf[1].buf = val;
-    rx_buf[1].len = count;
-    rx_buf_set.count = 2;
+	rx_buf[0].buf = NULL;
+	rx_buf[0].len = 2;
+	rx_buf[1].buf = val;
+	rx_buf[1].len = count;
+	rx_buf_set.count = 2;
 
-    tx_buf[0].buf = &reg;
-    tx_buf[0].len = 1;
-    tx_buf[1].buf = NULL;
-    tx_buf[1].len = count + 1;
-    tx_buf_set.count = 2;
+	tx_buf[0].buf = &reg;
+	tx_buf[0].len = 1;
+	tx_buf[1].buf = NULL;
+	tx_buf[1].len = count + 1;
+	tx_buf_set.count = 2;
 
-    res = spi_transceive(data->bmi088_acc_com_dev,
-                   &bmi088_acc_spi_conf,
-                   &tx_buf_set,
-                   &rx_buf_set);
+	res = spi_transceive(data->bmi088_com_dev,
+			     &bmi088_acc_spi_conf,
+			     &tx_buf_set,
+			     &rx_buf_set);
 
-    if (res != 0){
-        return res;
-    }
+	if (res != 0) {
+		return res;
+	}
 
-    return 0;
+	return 0;
 }
 
 int write_register(struct bmi088_acc_data *data, u16_t reg, int count,
-                   void *val)
+		   void *val)
 {
-    int res;
-    struct spi_buf_set tx_buf_set;
-    struct spi_buf_set rx_buf_set;
+	int res;
+	struct spi_buf_set tx_buf_set;
+	struct spi_buf_set rx_buf_set;
 
-    reg = reg & ~(1<<7);
+	reg = reg & ~(1 << 7);
 
-    struct spi_buf tx_buf[2] = {
-        {
-            .buf = &reg,
-            .len = 1,
-        },
-        {
-            .buf = val,
-            .len = count,
-        },
-    };
+	struct spi_buf tx_buf[2] = {
+		{
+			.buf = &reg,
+			.len = 1,
+		},
+		{
+			.buf = val,
+			.len = count,
+		},
+	};
 
-    struct spi_buf rx_buf = {
-        .buf = NULL,
-        .len = count + 1,
-    };
+	struct spi_buf rx_buf = {
+		.buf = NULL,
+		.len = count + 1,
+	};
 
-    tx_buf_set.buffers =  tx_buf;
-    tx_buf_set.count = 2;
-    rx_buf_set.buffers = &rx_buf;
-    rx_buf_set.count = 1;
+	tx_buf_set.buffers =  tx_buf;
+	tx_buf_set.count = 2;
+	rx_buf_set.buffers = &rx_buf;
+	rx_buf_set.count = 1;
 
-    res = spi_transceive(data->bmi088_com_dev,
-                         &bmi088_acc_spi_conf,
-                         &tx_buf_set,
-                         &rx_buf_set);
+	res = spi_transceive(data->bmi088_com_dev,
+			     &bmi088_acc_spi_conf,
+			     &tx_buf_set,
+			     &rx_buf_set);
 
-    if (res != 0) {
-        return res;
-    }
+	if (res != 0) {
+		return res;
+	}
 
-    return 0;
+	return 0;
 }
 
 static const struct bmi088_transfer_function bmi088_acc_spi_tf = {
-    .read_register = read_register,
-    .write_register = write_register,
+	.read_register = read_register,
+	.write_register = write_register,
 };
 
-int bmi088_spi_init(struct bmi088_data *data)
+int bmi088_acc_spi_init(struct bmi088_acc_data *data)
 {
-    data->tf = &bmi088_acc_spi_tf;
+	data->tf = &bmi088_acc_spi_tf;
 
-    if (IS_ENABLED(DT_INST_0_BOSCH_BMI088_ACCEL_CS_GPIOS_CONTROLLER)){
-        bmi088_acc_cs_ctrl.gpio_dev = device_get_binding(
-            DT_INST_0_BOSCH_BMI088_ACCEL_CS_GPIOS_CONTROLLER
-        );
+#if defined(DT_INST_0_BOSCH_BMI088_ACCEL_CS_GPIOS_CONTROLLER)
+	bmi088_acc_cs_ctrl.gpio_dev = device_get_binding(
+		DT_INST_0_BOSCH_BMI088_ACCEL_CS_GPIOS_CONTROLLER
+		);
 
-        if (!bmi088_acc_cs_ctrl.gpio_dev) {
-            LOG_ERR("Unable to get gpio device for chip select");
-            return -ENODEV;
-        }
+	if (!bmi088_acc_cs_ctrl.gpio_dev) {
+		LOG_ERR("Unable to get gpio device for chip select");
+		return -ENODEV;
+	}
 
-        bmi088_acc_cs_ctrl.gpio_pin = DT_INST_0_BOSCH_BMI088_ACCEL_CS_GPIOS_PIN;
-        bmi088_acc_cs_ctrl.delay = 1U;
-        bmi088_acc_spi_conf.cs = &bmi088_acc_cs_ctrl;
-    }
+	bmi088_acc_cs_ctrl.gpio_pin = DT_INST_0_BOSCH_BMI088_ACCEL_CS_GPIOS_PIN;
+	bmi088_acc_cs_ctrl.delay = 1U;
+	bmi088_acc_spi_conf.cs = &bmi088_acc_cs_ctrl;
+#endif
 
-    /* performe a dummy read of the id register to set the device to spi mode */
-    u8_t reg =  BMI088_REG_ACC_CHIP_ID;
-    u8_t reg_val;
+	/* performe a dummy id read to set the device to spi mode */
+	u8_t reg =  BMI088_REG_ACC_CHIP_ID;
+	u8_t reg_val;
 
-    int res = data->tf->read_register(data, reg, 1, &reg_val);
+	int res = data->tf->read_register(data, reg, 1, &reg_val);
 
-    return res;
+	return res;
 }
