bc8c512f665e44fb4d5f9db18729f8e7dac7f596
Thomas Schmid <tom@binary-kitchen.de>
2019-11-03 15:29:29 +0100

    bmi088_gyro: style fixes and minor reworks
    Signed-off-by: Thomas Schmid <tom@binary-kitchen.de>

--- drivers/sensor/bmi088_gyro/bmi088_gyro.h
+++ drivers/sensor/bmi088_gyro/bmi088_gyro.h
@@ -8,42 +8,42 @@
 
 #define UBIT(n) (1u << n)
 
-#define BMI088_REG_GYRO_SELF_TEST      (u8_t) 0x3C
-#define BMI088_REG_INT3_INT4_IO_MAP    (u8_t) 0x18
-#define BMI088_REG_INT3_INT4_IO_CONF   (u8_t) 0x16
-#define BMI088_REG_GYRO_INT_CTRL       (u8_t) 0x15
-#define BMI088_REG_GYR0_SOFTRESET      (u8_t) 0x14
-#define BMI088_REG_GYRO_LPM1           (u8_t) 0x11
-#define BMI088_REG_GYRO_BANDWITH       (u8_t) 0x10
-#define BMI088_REG_GYRO_RANGE          (u8_t) 0x0F
-#define BMI088_REG_GYRO_INT_STAT_1     (u8_t) 0x0A
-#define BMI088_REG_RATE_Z_MSB          (u8_t) 0x07
-#define BMI088_REG_RATE_Z_LSB          (u8_t) 0x06
-#define BMI088_REG_RATE_Y_MSB          (u8_t) 0x05
-#define BMI088_REG_RATE_Y_LSB          (u8_t) 0x04
-#define BMI088_REG_RATE_X_MSB          (u8_t) 0x03
-#define BMI088_REG_RATE_X_LSB          (u8_t) 0x02
-#define BMI088_REG_GYRO_CHIP_ID        (u8_t) 0x00
+#define BMI088_REG_GYRO_SELF_TEST      ((u8_t) 0x3C)
+#define BMI088_REG_INT3_INT4_IO_MAP    ((u8_t) 0x18)
+#define BMI088_REG_INT3_INT4_IO_CONF   ((u8_t) 0x16)
+#define BMI088_REG_GYRO_INT_CTRL       ((u8_t) 0x15)
+#define BMI088_REG_GYR0_SOFTRESET      ((u8_t) 0x14)
+#define BMI088_REG_GYRO_LPM1           ((u8_t) 0x11)
+#define BMI088_REG_GYRO_BANDWIDTH      ((u8_t) 0x10)
+#define BMI088_REG_GYRO_RANGE          ((u8_t) 0x0F)
+#define BMI088_REG_GYRO_INT_STAT_1     ((u8_t) 0x0A)
+#define BMI088_REG_RATE_Z_MSB          ((u8_t) 0x07)
+#define BMI088_REG_RATE_Z_LSB          ((u8_t) 0x06)
+#define BMI088_REG_RATE_Y_MSB          ((u8_t) 0x05)
+#define BMI088_REG_RATE_Y_LSB          ((u8_t) 0x04)
+#define BMI088_REG_RATE_X_MSB          ((u8_t) 0x03)
+#define BMI088_REG_RATE_X_LSB          ((u8_t) 0x02)
+#define BMI088_REG_GYRO_CHIP_ID        ((u8_t) 0x00)
 
 #define BMI088_MASK_GYRO_SELF_TEST_TRIG_BIST    UBIT(0)
 #define BMI088_MASK_GYRO_SELF_TEST_BIST_RDY     UBIT(1)
 #define BMI088_MASK_GYRO_SELF_TEST_BIST_FAIL    UBIT(2)
 #define BMI088_MASK_GYRO_SELF_TEST_RATE_OK      UBIT(4)
 
-#define BMI088_GYRO_RANGE_2000         (u8_t) 0x00
-#define BMI088_GYRO_RANGE_1000         (u8_t) 0x01
-#define BMI088_GYRO_RANGE_500          (u8_t) 0x02
-#define BMI088_GYRO_RANGE_250          (u8_t) 0x03
-#define BMI088_GYRO_RANGE_125          (u8_t) 0x04
+#define BMI088_GYRO_RANGE_2000         ((u8_t) 0x00)
+#define BMI088_GYRO_RANGE_1000         ((u8_t) 0x01)
+#define BMI088_GYRO_RANGE_500          ((u8_t) 0x02)
+#define BMI088_GYRO_RANGE_250          ((u8_t) 0x03)
+#define BMI088_GYRO_RANGE_125          ((u8_t) 0x04)
 
-#define BMI088_GYRO_BANDWITH_532       0x00
-#define BMI088_GYRO_BANDWITH_230       0x01
-#define BMI088_GYRO_BANDWITH_116       0x02
-#define BMI088_GYRO_BANDWITH_47        0x03
-#define BMI088_GYRO_BANDWITH_23        0x04
-#define BMI088_GYRO_BANDWITH_12        0x05
-#define BMI088_GYRO_BANDWITH_64        0x06
-#define BMI088_GYRO_BANDWITH_32        0x07
+#define BMI088_GYRO_BANDWIDTH_532       0x00
+#define BMI088_GYRO_BANDWIDTH_230       0x01
+#define BMI088_GYRO_BANDWIDTH_116       0x02
+#define BMI088_GYRO_BANDWIDTH_47        0x03
+#define BMI088_GYRO_BANDWIDTH_23        0x04
+#define BMI088_GYRO_BANDWIDTH_12        0x05
+#define BMI088_GYRO_BANDWIDTH_64        0x06
+#define BMI088_GYRO_BANDWIDTH_32        0x07
 
 #define BMI088_GYRO_LPM1_NORMAL        0x00
 #define BMI088_GYRO_LPM1_SUSPEND       0x80
@@ -64,64 +64,64 @@
 #endif
 
 #if defined(CONFIG_BMI_GYRO_ODR_2000_BW_532)
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_532
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_532
 #elif defined(BMI088_GYRO_ODR_2000_BW_230)
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_230
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_230
 #elif defined(BMI088_GYRO_ODR_2000_BW_116)
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_116
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_116
 #elif defined(BMI088_GYRO_ODR_2000_BW_47)
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_47
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_47
 #elif defined(BMI088_GYRO_ODR_2000_BW_23)
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_23
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_23
 #elif defined(BMI088_GYRO_ODR_2000_BW_64)
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_64
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_64
 #elif defined(BMI088_GYRO_ODR_2000_BW_32)
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_32
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_32
 #elif defined(BMI088_GYRO_ODR_2000_BW_12)
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_12
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_12
 #else
-#   define BMI088_GYRO_DEFAULT_BANDWITH    BMI088_GYRO_BANDWITH_230
+#   define BMI088_GYRO_DEFAULT_BANDWIDTH    BMI088_GYRO_BANDWIDTH_230
 #endif
 
 enum BMI088_GYRO_ATTR {
-    BMI088_GYRO_ATTR_BW = SENSOR_ATTR_PRIV_START + 1,
+	BMI088_GYRO_ATTR_BW = SENSOR_ATTR_PRIV_START,
 };
 
 struct bmi088_gyr_data {
-    struct device *bmi088_com_dev;
-    const struct bmi088_transfer_function *tf;
+	struct device *bmi088_com_dev;
+	const struct bmi088_transfer_function *tf;
 
-    u8_t gyro_odr;
-    u8_t gyro_range;
+	u8_t gyro_odr;
+	u8_t gyro_range;
 
-    union {
-        struct {
-            s16_t x_value;
-            s16_t y_value;
-            s16_t z_value;
-        }__attribute__((packed));
-        s16_t array[3];
-    } gyro_raw_values;
+	union {
+		struct {
+			s16_t x_value;
+			s16_t y_value;
+			s16_t z_value;
+		} __packed;
+		s16_t array[3];
+	} gyro_raw_values;
 
-    union {
-        struct {
-            struct sensor_value x_value;
-            struct sensor_value y_value;
-            struct sensor_value z_value;
-        }__attribute__((packed));
-        struct sensor_value array[3];
-    } gyro_values;
+	union {
+		struct {
+			struct sensor_value x_value;
+			struct sensor_value y_value;
+			struct sensor_value z_value;
+		} __packed;
+		struct sensor_value array[3];
+	} gyro_values;
 };
 
 struct bmi088_transfer_function {
-    int (*read_register)(struct bmi088_gyr_data *data, u16_t reg,
-                         int count, void *val);
-    int (*write_register)(struct bmi088_gyr_data *data, u16_t reg,
-                         int count, void *val);
+	int (*read_register)(struct bmi088_gyr_data *data, u16_t reg,
+			     int count, void *val);
+	int (*write_register)(struct bmi088_gyr_data *data, u16_t reg,
+			      int count, void *val);
 };
 
 struct bmi088_gyr_config {
-    char *bmi088_com_dev_name;
+	char *bmi088_com_dev_name;
 };
 
 int bmi088_gyr_spi_init(struct bmi088_gyr_data *data);
--- drivers/sensor/bmi088_gyro/bmi088_gyro_spi.c
+++ drivers/sensor/bmi088_gyro/bmi088_gyro_spi.c
@@ -10,116 +10,116 @@ static struct spi_cs_control bmi088_gyr_cs_ctrl;
 #endif
 
 static struct spi_config bmi088_gyr_spi_conf = {
-    .frequency = DT_INST_0_BOSCH_BMI088_GYRO_SPI_MAX_FREQUENCY,
-    .operation = (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) |
-                  SPI_MODE_CPOL | SPI_MODE_CPHA |
-                  SPI_TRANSFER_MSB |
-                  SPI_LINES_SINGLE),
-    .slave     = DT_INST_0_BOSCH_BMI088_GYRO_BASE_ADDRESS,
-    .cs        = SPI_CS,
+	.frequency = DT_INST_0_BOSCH_BMI088_GYRO_SPI_MAX_FREQUENCY,
+	.operation = (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) |
+		      SPI_MODE_CPOL | SPI_MODE_CPHA |
+		      SPI_TRANSFER_MSB |
+		      SPI_LINES_SINGLE),
+	.slave = DT_INST_0_BOSCH_BMI088_GYRO_BASE_ADDRESS,
+	.cs = SPI_CS,
 };
 
-int read_register(struct bmi088_gyr_data *data, u16_t reg, int count, void *val)
+int static read_register(struct bmi088_gyr_data *data, u16_t reg, int count, void *val)
 {
-    int res;
-    struct spi_buf_set rx_buf_set;
-    struct spi_buf_set tx_buf_set;
-    struct spi_buf tx_buf[2];
-    struct spi_buf rx_buf[2];
+	int res;
+	struct spi_buf_set rx_buf_set;
+	struct spi_buf_set tx_buf_set;
+	struct spi_buf tx_buf[2];
+	struct spi_buf rx_buf[2];
 
-    rx_buf_set.buffers = rx_buf;
-    tx_buf_set.buffers = tx_buf;
+	rx_buf_set.buffers = rx_buf;
+	tx_buf_set.buffers = tx_buf;
 
-    reg = reg | (1 << 7);
+	reg = reg | (1 << 7);
 
-    rx_buf[0].buf = NULL;
-    rx_buf[0].len = 1;
-    rx_buf[1].buf = val;
-    rx_buf[1].len = count;
-    rx_buf_set.count = 2;
+	rx_buf[0].buf = NULL;
+	rx_buf[0].len = 1;
+	rx_buf[1].buf = val;
+	rx_buf[1].len = count;
+	rx_buf_set.count = 2;
 
-    tx_buf[0].buf = &reg;
-    tx_buf[0].len = 1;
-    tx_buf[1].buf = NULL;
-    tx_buf[1].len = count;
-    tx_buf_set.count = 2;
+	tx_buf[0].buf = &reg;
+	tx_buf[0].len = 1;
+	tx_buf[1].buf = NULL;
+	tx_buf[1].len = count;
+	tx_buf_set.count = 2;
 
-    res = spi_transceive(data->bmi088_com_dev,
-                   &bmi088_gyr_spi_conf,
-                   &tx_buf_set,
-                   &rx_buf_set);
-    
-    if (res != 0){
-        return res;
-    }
+	res = spi_transceive(data->bmi088_com_dev,
+			     &bmi088_gyr_spi_conf,
+			     &tx_buf_set,
+			     &rx_buf_set);
 
-    return 0;
+	if (res != 0) {
+		return res;
+	}
+
+	return 0;
 }
 
-int write_register(struct bmi088_gyr_data *data, u16_t reg, int count,
-                   void *val)
+int static write_register(struct bmi088_gyr_data *data, u16_t reg, int count,
+			  void *val)
 {
-    int res;
-    struct spi_buf_set tx_buf_set;
-    struct spi_buf_set rx_buf_set;
+	int res;
+	struct spi_buf_set tx_buf_set;
+	struct spi_buf_set rx_buf_set;
 
-    reg = reg & ~(1<<7);
+	reg = reg & ~(1 << 7);
 
-    struct spi_buf tx_buf[2] = {
-        {
-            .buf = &reg,
-            .len = 1,
-        },
-        {
-            .buf = val,
-            .len = count,
-        },
-    };
+	struct spi_buf tx_buf[2] = {
+		{
+			.buf = &reg,
+			.len = 1,
+		},
+		{
+			.buf = val,
+			.len = count,
+		},
+	};
 
-    struct spi_buf rx_buf = {
-        .buf = NULL,
-        .len = count + 1,
-    };
+	struct spi_buf rx_buf = {
+		.buf = NULL,
+		.len = count + 1,
+	};
 
-    tx_buf_set.buffers =  tx_buf;
-    tx_buf_set.count = 2;
-    rx_buf_set.buffers = &rx_buf;
-    rx_buf_set.count = 1;
+	tx_buf_set.buffers =  tx_buf;
+	tx_buf_set.count = 2;
+	rx_buf_set.buffers = &rx_buf;
+	rx_buf_set.count = 1;
 
-    res = spi_transceive(data->bmi088_com_dev,
-                         &bmi088_gyr_spi_conf,
-                         &tx_buf_set,
-                         &rx_buf_set);
+	res = spi_transceive(data->bmi088_com_dev,
+			     &bmi088_gyr_spi_conf,
+			     &tx_buf_set,
+			     &rx_buf_set);
 
-    if (res != 0) {
-        return res;
-    }
+	if (res != 0) {
+		return res;
+	}
 
-    return 0;
+	return 0;
 }
 
 static const struct bmi088_transfer_function bmi088_gyr_spi_tf = {
-    .read_register = read_register,
-    .write_register = write_register,
+	.read_register = read_register,
+	.write_register = write_register,
 };
 
 int bmi088_gyr_spi_init(struct bmi088_gyr_data *data)
 {
-    data->tf = &bmi088_gyr_spi_tf;
+	data->tf = &bmi088_gyr_spi_tf;
 
-    if (IS_ENABLED(DT_INST_0_BOSCH_BMI088_GYRO_CS_GPIOS_CONTROLLER)){
-        bmi088_gyr_cs_ctrl.gpio_dev = device_get_binding(
-                        DT_INST_0_BOSCH_BMI088_GYRO_CS_GPIOS_CONTROLLER);
-    
-        if (!bmi088_gyr_cs_ctrl.gpio_dev) {
-            LOG_ERR("Unable to get gpio device for chip select");
-            return -ENODEV;
-        }
+#if defined(DT_INST_0_BOSCH_BMI088_GYRO_CS_GPIOS_CONTROLLER)
+	bmi088_gyr_cs_ctrl.gpio_dev = device_get_binding(
+		DT_INST_0_BOSCH_BMI088_GYRO_CS_GPIOS_CONTROLLER);
 
-        bmi088_gyr_cs_ctrl.gpio_pin = DT_INST_0_BOSCH_BMI088_GYRO_CS_GPIOS_PIN;
-        bmi088_gyr_cs_ctrl.delay = 1U;
-        bmi088_gyr_spi_conf.cs = &bmi088_gyr_cs_ctrl;
-    }
+	if (!bmi088_gyr_cs_ctrl.gpio_dev) {
+		LOG_ERR("Unable to get gpio device for chip select");
+		return -ENODEV;
+	}
 
-    return 0;
+	bmi088_gyr_cs_ctrl.gpio_pin = DT_INST_0_BOSCH_BMI088_GYRO_CS_GPIOS_PIN;
+	bmi088_gyr_cs_ctrl.delay = 1U;
+	bmi088_gyr_spi_conf.cs = &bmi088_gyr_cs_ctrl;
+#endif
+
+	return 0;
 }
